cef298506f4a9513ea05f9dd77c8f735ac1db6d0
S. Davilla <davilla@4pi.com>
2010-11-20 14:24:56 -0500

    refactor to include only what is needed on darwin platform

--- xbmc/linux/NetworkLinux.cpp
+++ xbmc/linux/NetworkLinux.cpp
@@ -24,18 +24,19 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #ifndef __APPLE__
-#include <linux/if.h>
-#include <linux/wireless.h>
-#include <linux/sockios.h>
+  #include <linux/if.h>
+  #include <linux/wireless.h>
+  #include <linux/sockios.h>
 #endif
 #include <errno.h>
 #include <resolv.h>
-#ifdef __APPLE__
-#include <sys/sockio.h>
-#include <net/if.h>
-#include <ifaddrs.h>
+#if defined(__APPLE__)
+  #include <sys/sockio.h>
+  #include <net/if.h>
+  #include <ifaddrs.h>
+#else
+  #include <net/if_arp.h>
 #endif
-#include <net/if_arp.h>
 #include "PlatformDefs.h"
 #include "NetworkLinux.h"
 #include "Util.h"
