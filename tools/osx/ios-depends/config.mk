e86db10fcb76c9c28b3e2647d6f3b7698696cb6f
S. Davilla <davilla@4pi.com>
2010-11-20 20:27:11 -0500

    add main Makefile and remove usage of sudo

--- tools/osx/ios-depends/config.mk
+++ tools/osx/ios-depends/config.mk
@@ -6,15 +6,7 @@ platform_path=/Developer/Platforms/iPhoneOS.platform/Developer
 platform_os_min=iphoneos-version-min=${platform_os_version}
 platform_os_cflags=-march=armv7 -mcpu=cortex-a8 -mfpu=neon -ftree-vectorize -mfloat-abi=softfp -mdynamic-no-pic -pipe -Wno-trigraphs -fpascal-strings -O0 -Wreturn-type -Wunused-variable -fmessage-length=0 -gdwarf-2
 platform_os_ldflags=-march=armv7 -mcpu=cortex-a8
-prefix_path=/usr/local/xbmc-ios-${platform_os_version}
-
-#platform_os="MacOSX"
-#platform_os_version="10.4"
-#platform_path="/Developer"
-#platform_os_min="macosx-version-min=${platform_os_version}"
-#platform_os_cflags=""
-#prefix_path="/usr/local/xbmc-osx-${platform_os_version}"
-
+prefix_path=`pwd`/../../../build/xbmc-ios-${platform_os_version}
 platform_sdk_path=${platform_path}/SDKs/${platform_os}${platform_os_version}.sdk
 platform_bin_path=${platform_path}/usr/bin
 
