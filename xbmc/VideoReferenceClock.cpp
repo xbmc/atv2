cdf6c505e7a60a4ff294876a1054f073f64750ba
S. Davilla <davilla@4pi.com>
2010-11-26 20:05:09 -0500

    refactor displaylink code and enabled for arm

--- xbmc/VideoReferenceClock.cpp
+++ xbmc/VideoReferenceClock.cpp
@@ -33,6 +33,8 @@
   #define NVSETTINGSCMD "nvidia-settings -nt -q RefreshRate3"
 #elif defined(__APPLE__) && !defined(__arm__)
   #include "CocoaInterface.h"
+#elif defined(__APPLE__) && defined(__arm__)
+  #include "WindowingFactory.h"
 #elif defined(_WIN32) && defined(HAS_DX)
   #pragma comment (lib,"d3d9.lib")
   #if (D3DX_SDK_VERSION >= 42) //aug 2009 sdk and up there is no dxerr9 anymore
@@ -131,7 +133,7 @@ void CVideoReferenceClock::Process()
     SetupSuccess = SetupGLX();
 #elif defined(_WIN32) && defined(HAS_DX)
     SetupSuccess = SetupD3D();
-#elif defined(__APPLE__) && !defined(__arm__)
+#elif defined(__APPLE__)
     SetupSuccess = SetupCocoa();
 #elif defined(HAS_GLX)
     CLog::Log(LOGDEBUG, "CVideoReferenceClock: compiled without RandR support");
@@ -161,7 +163,7 @@ void CVideoReferenceClock::Process()
       RunGLX();
 #elif defined(_WIN32) && defined(HAS_DX)
       RunD3D();
-#elif defined(__APPLE__) && !defined(__arm__)
+#elif defined(__APPLE__)
       RunCocoa();
 #endif
 
@@ -183,7 +185,7 @@ void CVideoReferenceClock::Process()
     CleanupGLX();
 #elif defined(_WIN32) && defined(HAS_DX)
     CleanupD3D();
-#elif defined(__APPLE__) && !defined(__arm__)
+#elif defined(__APPLE__)
     CleanupCocoa();
 #endif
     if (!SetupSuccess) break;
@@ -758,8 +760,8 @@ void CVideoReferenceClock::CleanupD3D()
   m_D3dCallback.Release();
 }
 
-#elif defined(__APPLE__) && !defined(__arm__)
-
+#elif defined(__APPLE__)
+#if !defined(__arm__)
 // Called by the Core Video Display Link whenever it's appropriate to render a frame.
 static CVReturn DisplayLinkCallBack(CVDisplayLinkRef displayLink, const CVTimeStamp* inNow, const CVTimeStamp* inOutputTime, CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext)
 {
@@ -779,7 +781,7 @@ static CVReturn DisplayLinkCallBack(CVDisplayLinkRef displayLink, const CVTimeSt
 
   return kCVReturnSuccess;
 }
-
+#endif
 bool CVideoReferenceClock::SetupCocoa()
 {
   CLog::Log(LOGDEBUG, "CVideoReferenceClock: setting up Cocoa");
@@ -789,12 +791,18 @@ bool CVideoReferenceClock::SetupCocoa()
   m_MissedVblanks = 0;
   m_RefreshRate = 60;              //init the refreshrate so we don't get any division by 0 errors
 
+  #if defined(__arm__)
+  {
+    g_Windowing.InitDisplayLink();
+  }
+  #else
   if (!Cocoa_CVDisplayLinkCreate((void*)DisplayLinkCallBack, reinterpret_cast<void*>(this)))
   {
     CLog::Log(LOGDEBUG, "CVideoReferenceClock: Cocoa_CVDisplayLinkCreate failed");
     return false;
   }
   else
+  #endif
   {
     UpdateRefreshrate(true);
     return true;
@@ -813,9 +821,14 @@ void CVideoReferenceClock::RunCocoa()
 void CVideoReferenceClock::CleanupCocoa()
 {
   CLog::Log(LOGDEBUG, "CVideoReferenceClock: cleaning up Cocoa");
-  Cocoa_CVDisplayLinkRelease();
+  #if defined(__arm__)
+    g_Windowing.DeinitDisplayLink();
+  #else
+    Cocoa_CVDisplayLinkRelease();
+  #endif
 }
 
+#endif
 void CVideoReferenceClock::VblankHandler(int64_t nowtime, double fps)
 {
   int           NrVBlanks;
@@ -846,7 +859,6 @@ void CVideoReferenceClock::VblankHandler(int64_t nowtime, double fps)
 
   SendVblankSignal();
 }
-#endif
 
 //this is called from the vblank run function and from CVideoReferenceClock::Wait in case of a late update
 void CVideoReferenceClock::UpdateClock(int NrVBlanks, bool CheckMissed)
@@ -1014,8 +1026,13 @@ bool CVideoReferenceClock::UpdateRefreshrate(bool Forced /*= false*/)
 
   return false;
 
-#elif defined(__APPLE__) && !defined(__arm__)
-  int RefreshRate = MathUtils::round_int(Cocoa_GetCVDisplayLinkRefreshPeriod());
+#elif defined(__APPLE__)
+  #if defined(__arm__)
+    int RefreshRate = round(g_Windowing.GetDisplayLinkFPS() + 0.5);
+    //int RefreshRate = MathUtils::round_int(g_Windowing.GetDisplayLinkFPS());
+  #else
+    int RefreshRate = MathUtils::round_int(Cocoa_GetCVDisplayLinkRefreshPeriod());
+  #endif
 
   if (RefreshRate != m_RefreshRate || Forced)
   {
