b4be726e416957f6d2d9e03646e6021610023b25
S. Davilla <davilla@4pi.com>
2010-11-27 20:03:03 -0500

    temp change to test shader speed

--- system/shaders/yuv2rgb_basic_gles.glsl
+++ system/shaders/yuv2rgb_basic_gles.glsl
@@ -31,10 +31,13 @@ uniform mat4      m_yuvmat;
 
 void main()
 {
+/*
   vec4 yuv, rgb;
   yuv.rgba = vec4(texture2D(m_sampY, m_cordY).r, texture2D(m_sampU, m_cordU).r, texture2D(m_sampV, m_cordV).r, 1.0);
 
   rgb   = m_yuvmat * yuv;
   rgb.a = m_alpha;
   gl_FragColor = rgb;
+*/
+  gl_FragColor = vec4(texture2D(m_sampY, m_cordY).r);
 }
