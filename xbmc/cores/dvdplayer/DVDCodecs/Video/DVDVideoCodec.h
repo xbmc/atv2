487a8ce23abd015ab5987a6d209fc7c00fa8f48c
S. Davilla <davilla@4pi.com>
2010-12-06 16:55:32 -0500

    merge in xbmc-for-tegra2 changes to egl rendering, this will need more clean up

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodec.h
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodec.h
@@ -35,6 +35,8 @@
 namespace DXVA { class CProcessor; }
 namespace VAAPI { class CHolder; }
 class CVDPAU;
+class COpenMaxVideo;
+struct OpenMaxVideoBuffer;
 
 // should be entirely filled by all codecs
 struct DVDVideoPicture
@@ -57,6 +59,11 @@ struct DVDVideoPicture
     };
     struct {
       VAAPI::CHolder* vaapi;
+    };
+
+    struct {
+      COpenMaxVideo *openMax;
+      OpenMaxVideoBuffer *openMaxBuffer;
     };
   };
 
@@ -86,6 +93,7 @@ struct DVDVideoPicture
     FMT_YUY2,
     FMT_DXVA,
     FMT_VAAPI,
+    FMT_OMXEGL
   } format;
 };
 
