d2702ecf27cfa0c6ec5ad57f74c4a5bd3803bad1
S. Davilla <davilla@4pi.com>
2011-01-07 15:54:15 -0500

    removed xbmcclient from ios, not used

--- xbmc/osx/ios/XBMCController.mm
+++ xbmc/osx/ios/XBMCController.mm
@@ -44,35 +44,8 @@
 
 #import "XBMCController.h"
 #import "XBMCApplication.h"
-#import "xbmcclientwrapper.h"
 #import "XBMCDebugHelpers.h"
 #import "iOSUtils.h"
-
-typedef enum {
-  // for originator kBREventOriginatorRemote
-  kBREventRemoteActionMenu = 1,
-  kBREventRemoteActionMenuHold,
-  kBREventRemoteActionUp,
-  kBREventRemoteActionDown,
-  kBREventRemoteActionPlay,
-  kBREventRemoteActionLeft,
-  kBREventRemoteActionRight,
-
-  kBREventRemoteActionPlayHold = 20,
-
-  // Gestures, for originator kBREventOriginatorGesture
-  kBREventRemoteActionTap = 30,
-  kBREventRemoteActionSwipeLeft,
-  kBREventRemoteActionSwipeRight,
-  kBREventRemoteActionSwipeUp,
-  kBREventRemoteActionSwipeDown,
-
-  // Custom remote actions for old remote actions
-  kBREventRemoteActionHoldLeft = 0xfeed0001,
-  kBREventRemoteActionHoldRight,
-  kBREventRemoteActionHoldUp,
-  kBREventRemoteActionHoldDown,
-} BREventRemoteAction;
 
 XBMCController *g_xbmcController;
 
@@ -292,17 +265,11 @@ extern NSString* kBRScreenSaverDismissed;
   self.displayLink = nil;
 	
   g_xbmcController = self;
-  
-  bool use_universal = NO;
-  NSString *client_address= @"localhost";
-  mp_xbmclient = [[XBMCClientWrapper alloc] initWithUniversalMode:use_universal serverAddress:client_address];
 }
 
 - (void)dealloc
 {
   NSLog(@"%s", __PRETTY_FUNCTION__);
-
-	[mp_xbmclient release];
 
   NSNotificationCenter *center;
   // take us off the default center for our app
@@ -553,108 +520,6 @@ extern NSString* kBRScreenSaverDismissed;
     [super didReceiveMemoryWarning];
 	
     // Release any cached data, images, etc. that aren't in use.
-}
-
-- (eATVClientEvent) ATVClientEventFromBREvent:(BREvent*) f_event
-{
-  BOOL downEvent = [f_event value];
-  int remoteAction = [f_event remoteAction];
-  DLOG(@"got action %i %@", remoteAction, (downEvent)? @"pressed":@"released");
-  
-  switch (remoteAction)
-  {
-    case kBREventRemoteActionUp:
-    case 65676:  // tap up
-      if([f_event value] == 1)
-        return ATV_BUTTON_UP;
-      else
-        return ATV_BUTTON_UP_RELEASE;
-    case kBREventRemoteActionDown:
-    case 65677:  // tap down
-      if([f_event value] == 1)
-        return ATV_BUTTON_DOWN;
-      else
-        return ATV_BUTTON_DOWN_RELEASE;
-    case kBREventRemoteActionLeft:
-    case 65675:  // tap left
-      if([f_event value] == 1)
-        return ATV_BUTTON_LEFT;
-      else
-        return ATV_BUTTON_LEFT_RELEASE;
-    case 786612: // hold left
-      if([f_event value] == 1)
-        return ATV_LEARNED_REWIND;
-      else
-        return ATV_LEARNED_REWIND_RELEASE;
-    case kBREventRemoteActionRight:
-    case 65674:  // tap right
-      if ([f_event value] == 1)
-        return ATV_BUTTON_RIGHT;
-      else
-        return ATV_BUTTON_RIGHT_RELEASE;
-    case 786611: // hold right
-      if ([f_event value] == 1)
-        return ATV_LEARNED_FORWARD;
-      else
-        return ATV_LEARNED_FORWARD_RELEASE;
-    case kBREventRemoteActionPlay:
-    case 65673:  // tap play
-      return ATV_BUTTON_PLAY;
-    case kBREventRemoteActionPlayHold:
-    case 65668:  // hold play
-      return ATV_BUTTON_PLAY_H;
-    case kBREventRemoteActionMenu:
-    case 65670:  // menu
-      return ATV_BUTTON_MENU;
-    case kBREventRemoteActionMenuHold:
-    case 786496: // hold menu
-      return ATV_BUTTON_MENU_H;
-    case 786608: //learned play
-      return ATV_LEARNED_PLAY;
-    case 786609: //learned pause
-      return ATV_LEARNED_PAUSE;
-    case 786615: //learned stop
-      return ATV_LEARNED_STOP;
-    case 786613: //learned nexxt
-      return ATV_LEARNED_NEXT;
-    case 786614: //learned previous
-      return ATV_LEARNED_PREVIOUS;
-    case 786630: //learned enter, like go into something
-      return ATV_LEARNED_ENTER;
-    case 786631: //learned return, like go back
-      return ATV_LEARNED_RETURN;
-    case 786637:
-      return ATV_ALUMINIUM_PLAY;
-    default:
-      ELOG(@"XBMCPureController: Unknown button press remoteAction = %i", remoteAction);
-      return ATV_INVALID_BUTTON;
-  }
-}
-
-- (BOOL)brEventAction:(BREvent*)event
-{
-  NSLog(@"%s", __PRETTY_FUNCTION__);
-
-  if (animating)
-  {
-    eATVClientEvent xbmcclient_event = [self ATVClientEventFromBREvent:event];
-    
-    if ( xbmcclient_event == ATV_INVALID_BUTTON )
-    {
-      return NO;
-    } 
-    else
-    {
-      [mp_xbmclient handleEvent:xbmcclient_event];
-      return TRUE;
-    }
-  }
-  else
-  {
-    // TODO 
-    //return [super brEventAction:event];
-	return FALSE;
-  }
 }
 
 #pragma mark -
