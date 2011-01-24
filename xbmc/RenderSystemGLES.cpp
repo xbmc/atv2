d3ad79726e257040b300a3aba1b3e8b718c43552
S. Davilla <davilla@4pi.com>
2011-01-23 19:42:41 -0500

    he shrugs as to why git thinks this needs a commit, yet git diff shows nothing

--- xbmc/RenderSystemGLES.cpp
+++ xbmc/RenderSystemGLES.cpp
@@ -40,7 +40,7 @@ static const char* ShaderNames[SM_ESHADERCOUNT] =
      "guishader_frag_texture_noblend.glsl",
      "guishader_frag_multi_blendcolor.glsl",
      "guishader_frag_texture_rgba.glsl",
-     "guishader_frag_texture_noblend_rgba.glsl"
+     "guishader_frag_texture_noblend_rgba.glsl",
      };
 
 CRenderSystemGLES::CRenderSystemGLES()
