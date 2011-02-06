d538b07ded825ca5b8120da700a3cc24c44baae7
S. Davilla <davilla@4pi.com>
2011-02-06 09:46:11 -0500

    bump and sync version numbers for atv2/ios

--- tools/osx/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
+++ tools/osx/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
@@ -16,7 +16,7 @@ fi
 PACKAGE=org.xbmc.xbmc-ios
 
 VERSION=10.0
-REVISION=1
+REVISION=4
 ARCHIVE=${PACKAGE}_${VERSION}-${REVISION}_iphoneos-arm.deb
 
 echo Creating $PACKAGE package version $VERSION revision $REVISION
