4b032becc3005f7cf87102a661e46436ad3376c7
amet <amet.nospam@gmail.com>
2011-02-19 23:11:37 +0400

    bump revision for 10.0-7 release

--- tools/osx/packaging/xbmc-atv2/mkdeb-xbmc-atv2.sh
+++ tools/osx/packaging/xbmc-atv2/mkdeb-xbmc-atv2.sh
@@ -16,7 +16,7 @@ fi
 PACKAGE=org.xbmc.xbmc-atv2
 
 VERSION=10.0
-REVISION=6
+REVISION=7
 ARCHIVE=${PACKAGE}_${VERSION}-${REVISION}_iphoneos-arm.deb
 
 echo Creating $PACKAGE package version $VERSION revision $REVISION
